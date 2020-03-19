75a32368b69f398d6c653b2ba2ff8ae3bec07c64
timmy471 <ayantunjitimilehin@yahoo.com>
2020-03-19 17:10:36 +0100

    commit

--- db.php
+++ db.php
@@ -1,3 +1,6 @@
 <?php
-$mysqli = new mysqli('heroku_32566d8beeb6326', 'root', 'bd5859b5036734', 'ampcyclers');
+$mysqli = new mysqli('us-cdbr-iron-east-04.cleardb.net', 'bd5859b5036734', 'db74f49c', 'heroku_32566d8beeb6326');
 
+// /bd5859b5036734:db74f49c@us-cdbr-iron-east-04.cleardb.net/heroku_32566d8beeb6326?reconnect=true
+
+// host, username, pas, db name
